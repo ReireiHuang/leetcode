9ea4d0a632df2ccf3fe8bde05d77bc38a4231bed
Bill <bill25961073@gmail.com>
2021-07-17 23:07:13 +0800

    add RemoveDuplicates.cs

---
+++ RemoveDuplicates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    class Solution
+    {
+        public int RemoveDuplicates(int[] nums = null)
+        {
+            int count = 1;
+            if (nums.Length > 1)
+            {
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    if (nums[i] == nums[count - 1])
+                        continue;
+                    nums[count] = nums[i];
+                    count++;
+                }
+                return count;
+            }
+            return nums.Length;
+        }
+    }
+}
