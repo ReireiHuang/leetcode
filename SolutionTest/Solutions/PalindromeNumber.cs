f5ccde530fadfd2e9662a60df3fa59b174721e17
DESKTOP-24QQ4C0\Administrator <bill25961073@gmail.com>
2022-08-30 15:17:19 +0800

    Add PalindromeNumber Solution

---
+++ SolutionTest/Solutions/PalindromeNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Leetcode
+{
+    public class PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+                return false;
+
+            string a = x.ToString();
+            string b = "";
+            
+            while ((x / 10) > 0)
+            {
+                b += x % 10;
+                x /= 10;
+            }
+            b += x;
+
+            if(b == a)
+                return true;
+
+            return false;
+        }
+    }
+}
