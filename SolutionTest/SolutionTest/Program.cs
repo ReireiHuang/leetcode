a544113426ec433b8c5383db6bc4ddb82992ec75
DESKTOP-24QQ4C0\Administrator <bill25961073@gmail.com>
2022-08-31 18:25:32 +0800

    Add TestCase

--- SolutionTest/SolutionTest/Program.cs
+++ SolutionTest/SolutionTest/Program.cs
@@ -1,6 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Leetcode;
+using LeetCode.Common;
+int input = (int)LeetCodeSolutions.AddTwoNumber;
 
-PalindromeNumber p = new PalindromeNumber();
-Console.WriteLine(p.IsPalindrome(121));
+switch (input)
+{
+    case (int)LeetCodeSolutions.PalindromeNumber:
+        PalindromeNumber s = new PalindromeNumber();
+        Console.WriteLine(s.IsPalindrome(121));
+        break;
+    case (int)LeetCodeSolutions.AddTwoNumber:
+        AddTwoNumber s1 = new AddTwoNumber();
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        Console.WriteLine(s1.AddTwoNumbers(l1,l2).ShowNode());
+        break;
+    default:
+        break;
+}
+
+enum LeetCodeSolutions
+{
+    PalindromeNumber,
+    AddTwoNumber
+}
