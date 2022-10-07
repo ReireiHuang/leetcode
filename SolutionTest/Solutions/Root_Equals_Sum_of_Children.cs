4f115d902e6366826d9f6eee890f92043d7d690e
DESKTOP-24QQ4C0\Administrator <bill25961073@gmail.com>
2022-10-07 16:20:46 +0800

    Root Equals Sum of Children Solution

---
+++ SolutionTest/Solutions/Root_Equals_Sum_of_Children.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class Root_Equals_Sum_of_Children
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public bool CheckTree(TreeNode root)
+        {
+            return root.left.val + root.right.val == root.val;
+        }
+    }
+}
