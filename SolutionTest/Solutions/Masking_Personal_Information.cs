1c6a41d0572e91b085413e58e48279637fec3b01
DESKTOP-24QQ4C0\Administrator <bill25961073@gmail.com>
2022-10-07 16:21:19 +0800

    Masking Personal Information Solution

---
+++ SolutionTest/Solutions/Masking_Personal_Information.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class Masking_Personal_Information
+    {
+        public string MaskPII(string s)
+        {
+            if (s.IndexOf("@") > 0)
+            {
+                int idx = s.IndexOf("@");
+                s = s.ToLower();
+                return s[0] + "*****" + s[idx - 1] + s.Substring(idx);
+            }
+            else
+            {
+                s = s.Replace("+", "").Replace("-", "").Replace("(", "").Replace(")", "").Replace(" ", "");
+                int digit = s.Length - 10;
+                StringBuilder sb = new StringBuilder();
+                if (digit == 1)
+                {
+                    sb.Append("+*-");
+                }
+                else if (digit == 2)
+                {
+                    sb.Append("+**-");
+                }
+                else if (digit == 3)
+                {
+                    sb.Append("+***-");
+                }
+                sb.Append("***-***-");
+                sb.Append(s[s.Length - 4].ToString());
+                sb.Append(s[s.Length - 3].ToString());
+                sb.Append(s[s.Length - 2].ToString());
+                sb.Append(s[s.Length - 1].ToString());
+                return sb.ToString();
+            }
+        }
+    }
+}
