c3431b923bb4d77a9156e3cef0b9d073b6fc41c2
DESKTOP-24QQ4C0\Administrator <bill25961073@gmail.com>
2022-10-07 16:20:17 +0800

    Maximum Number of Words Found in Sentences Solution

---
+++ SolutionTest/Solutions/Maximum_Number_of_Words_Found_in_Sentences.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Solutions
+{
+    public class Maximum_Number_of_Words_Found_in_Sentences
+    {
+        public int MostWordsFound(string[] sentences)
+        {
+            int MaxWords = 0;
+            foreach (string content in sentences)
+            {
+                string[] splitContent = content.Split(' ');
+                if (MaxWords > splitContent.Length)
+                    continue;
+                MaxWords = splitContent.Length;
+            }
+            return MaxWords;
+        }
+    }
+    
+}
